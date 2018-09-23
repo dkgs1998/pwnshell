d715025927d80e40d0bdabfbe55e0f9fe76fe617
Zarchop <stalmps@gmail.com>
2018-09-24 01:06:40 +0300

    raw output mechanism change
    made raw output use hexdump instead of plainly using string_encode
    removed signal handler for sigchild, put try catch block around io from process in case we close it (with ctrl d for example) so we exit cleanly
    fixed some minor issue where under some circumstances the shell would not print CR as it should

--- main.py
+++ main.py
@@ -21,41 +21,38 @@ def exit_handler():
 	global termios
 	os.write(1, "\r{0} pwnshell out\n\r\n".format(MINUS))
 	termios.tcsetattr(0, termios.TCSANOW, term_attr)
-
-def child_die(sig_num, frame):
-	exit(0)
-
+	
 
 NORMAL_OUTPUT_MODE = 0
-HEX_OUTPUT_MODE_MIXED = 1
-HEX_OUTPUT_MODE_FULL = 2
+HEXDUMP_OUTPUT_MODE = 1
 raw_output_mode = NORMAL_OUTPUT_MODE
 def raw_output_mode_toggle():
 	global raw_output_mode
 	raw_output_mode += 1
-	raw_output_mode %= 3
+	raw_output_mode %= 2
 	if raw_output_mode == NORMAL_OUTPUT_MODE:
 		os.write(1, "\r\n{0} normal output mode\r\n".format(MINUS))
-	elif raw_output_mode == HEX_OUTPUT_MODE_MIXED:
-		os.write(1, "\r\n{0} raw (mixed) output mode\r\n".format(ASTERIX))
-	elif raw_output_mode == HEX_OUTPUT_MODE_FULL:
-		os.write(1, "\r\n{0} raw (full) output mode\r\n".format(PLUS))
+	elif raw_output_mode == HEXDUMP_OUTPUT_MODE:
+		os.write(1, "\r\n{0} hexdump output mode\r\n".format(PLUS))
 
 atexit.register(exit_handler)
-signal.signal(signal.SIGCHLD, child_die)
 
 os.write(1, "\n\r{0} welcome to pwnshell! ctrl+s to enter raw input mode, ctrl+c in raw input mode to cancel raw input mode and discard changes.".format(PLUS))
-os.write(1, "\r\n{0} ctrl+q to toggle raw output mode between normal, mixed hex and ascii mode or full hex mode.\r\n".format(PLUS))
+os.write(1, "\r\n{0} ctrl+q to toggle raw output mode between normal and hexdump mode.\r\n".format(PLUS))
 ret = pty.fork()
 if ret[0] == 0:
 	os.execv("/bin/bash", ["bash"])
 else:
+	shell_pid = ret[0]
 	tty.setraw(0)
 	pty=ret[1]
 	while True:
 		pending_fds = select.select([0, pty],[], [])
 		if pending_fds[0][0] == 0:
-			input_str = os.read(0, 0x1000000)
+			try:
+				input_str = os.read(0, 0x1000000)
+			except OSError:
+				exit(0)
 			if "\x13" in input_str: # ctrl+S
 				termios.tcsetattr(0, termios.TCSANOW, term_attr)
 				try:
@@ -72,16 +69,19 @@ else:
 				os.write(pty, in_buf.decode("string_escape"))
 				time.sleep(0.1)
 				termios.tcsetattr(pty, termios.TCSANOW, temp_attr)
-			elif "\x11" in input_str:
+			elif "\x11" in input_str: #ctrl+Q
 				raw_output_mode_toggle()
 			else:
 				os.write(pty, input_str)
 		else:
-			output_str = os.read(pty, 0x1000000)
-			if raw_output_mode == HEX_OUTPUT_MODE_MIXED:
-				output_str = output_str.encode("string_escape")
-			elif raw_output_mode == HEX_OUTPUT_MODE_FULL:
-				output_str = "\\x".join("{:02x}".format(ord(c)) for c in output_str)
-				output_str = "\\x" + output_str
-			os.write(1,output_str)
+			termios.tcsetattr(0, termios.TCSANOW, term_attr)
+			try:
+				output_str = os.read(pty, 0x1000000)
+			except OSError:
+				exit(0)
+			if raw_output_mode == HEXDUMP_OUTPUT_MODE:
+				os.system('echo "'+output_str+'" | hexdump -C')
+			else:
+				os.write(1,output_str)
+			tty.setraw(0)
 			

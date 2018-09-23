74c15c5952c3f3fdc7f738e9a0d4efbb1a4b706a
Zarchop <stalmps@gmail.com>
2018-09-23 19:41:44 +0300

    added support for full hex output mode

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ term_attr = termios.tcgetattr(0)
 def exit_handler():
 	global term_attr
 	global termios
-	os.write(1, "\r{0} pwnshell out\r\n".format(MINUS))
+	os.write(1, "\r{0} pwnshell out\n\r\n".format(MINUS))
 	termios.tcsetattr(0, termios.TCSANOW, term_attr)
 
 def child_die(sig_num, frame):
@@ -28,21 +28,24 @@ def child_die(sig_num, frame):
 
 NORMAL_OUTPUT_MODE = 0
 HEX_OUTPUT_MODE_MIXED = 1
+HEX_OUTPUT_MODE_FULL = 2
 raw_output_mode = NORMAL_OUTPUT_MODE
 def raw_output_mode_toggle():
 	global raw_output_mode
 	raw_output_mode += 1
-	raw_output_mode %= 2
+	raw_output_mode %= 3
 	if raw_output_mode == NORMAL_OUTPUT_MODE:
 		os.write(1, "\r\n{0} normal output mode\r\n".format(MINUS))
 	elif raw_output_mode == HEX_OUTPUT_MODE_MIXED:
 		os.write(1, "\r\n{0} raw (mixed) output mode\r\n".format(PLUS))
+	elif raw_output_mode == HEX_OUTPUT_MODE_FULL:
+		os.write(1, "\r\n{0} raw (full) output mode\r\n".format(PLUS))
 
 atexit.register(exit_handler)
 signal.signal(signal.SIGCHLD, child_die)
 
 os.write(1, "\n\r{0} welcome to pwnshell! ctrl+s to enter raw input mode, ctrl+c in raw input mode to cancel raw input mode and discard changes.".format(PLUS))
-os.write(1, "\r\n{0} ctrl+q to toggle raw output mode \r\n".format(PLUS))
+os.write(1, "\r\n{0} ctrl+q to toggle raw output mode between normal, mixed hex and ascii mode or full hex mode.\r\n".format(PLUS))
 ret = pty.fork()
 if ret[0] == 0:
 	os.execv("/bin/bash", ["bash"])
@@ -77,5 +80,8 @@ else:
 			output_str = os.read(pty, 0x1000000)
 			if raw_output_mode == HEX_OUTPUT_MODE_MIXED:
 				output_str = output_str.encode("string_escape")
+			elif raw_output_mode == HEX_OUTPUT_MODE_FULL:
+				output_str = "\\x".join("{:02x}".format(ord(c)) for c in output_str)
+				output_str = "\\x" + output_str
 			os.write(1,output_str)
 			
